572e963fe9d67f556253a0f327bf76fee3c82654
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-11-08 17:35:01 +0900

    added rspec config files

--- spec/models/account_type_spec.rb
+++ spec/models/account_type_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AccountType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect( FactoryGirl.build(:account_type) ).to be_valid 
+  end
+
+  describe "ActiveModel Validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+  end
 end

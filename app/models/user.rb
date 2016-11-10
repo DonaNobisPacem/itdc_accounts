511d5cda20603bb2625ef81f747d0bb04fdafde6
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-11-10 18:35:36 +0900

    added back provided and uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
     if user.present?
       unless user.provider.present? || user.uid.present?
         # user.email = auth.info.email
+        user.provider = auth.provider
+        user.uid = auth.uid
         user.password = Devise.friendly_token[0,20]
         user.first_name = auth.info.first_name   # assuming the user model has a name
         user.last_name = auth.info.last_name

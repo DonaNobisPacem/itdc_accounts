7e1ef8a03e4ff7222217c8adcae398e620c1bed3
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-11-15 14:03:13 +0900

    setup mailer views

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: 'info.ovpd@up.edu.ph'
   layout 'mailer'
+  
+  def hello_world
+    mail(to: 'johnthomasraphael@yahoo.com', subject: 'Mailer works!')
+  end
+
+  def welcome_email(user)
+    @user = user
+    @url  = '45.58.38.202'
+    mail(to: @user.email, subject: 'Welcome to the Accounts Management System')
+  end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def sample_hello_world_preview
+    UserMailer.hello_world
+  end
 
+  def sample_welcome_email_preview
+    UserMailer.welcome_email(User.first)
+  end
 end

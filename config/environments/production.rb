ff14f5ac7474da38064a26916afa58f29bf37595
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-11-18 15:04:48 +0900

    temp debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,6 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -77,6 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'ams.up-ovpd.ph' }
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
@@ -85,7 +85,7 @@ Rails.application.configure do
     :port                 => 587,
     :user_name            => ENV['smtp_username'],
     :password             => ENV['smtp_password'],
-    :authentication       => "plain",
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
 end

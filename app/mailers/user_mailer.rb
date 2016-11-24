f704c69dafae92f626874bb18a4281bce9d15444
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-11-24 13:01:50 +0900

    added account_update to usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,16 @@ class UserMailer < ApplicationMailer
     @url  = 'http://ams.up-ovpd.ph'
     mail(to: @user.email, subject: 'Welcome to the Accounts Management System')
   end
+
+  def account_update(user, admin, account)
+    @user = user
+    @user_name = @user.first_name.present? ? @user.first_name : @user.email
+    @admin = admin
+    @admin_name = @admin.first_name.present? ? @admin.first_name : @admin.email
+
+    @account = account
+    @url  = 'http://ams.up-ovpd.ph'
+    
+    mail(to: @user.email, subject: 'Your AMS account has been updated')
+  end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -7,4 +7,14 @@ class UserMailerPreview < ActionMailer::Preview
   def sample_welcome_email_preview
     UserMailer.welcome_email(User.first)
   end
+
+  def sample_account_update_preview
+    @user = FactoryGirl.create(:user, first_name: "Regular")
+    @admin = FactoryGirl.create(:user)
+
+    @account_type = FactoryGirl.create(:account_type)
+    @account = FactoryGirl.create(:account, user: @user, balance: 1000.00, account_type: @account_type.id)
+
+    UserMailer.account_update(@user, @admin, @account)
+  end
 end

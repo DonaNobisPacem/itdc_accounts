b2b1f3a441ade0649c209d0bc6b2b739aaa05a9e
DonaNobisPacem <johnthomasraphael@yahoo.com>
2016-12-01 13:29:15 +0900

    restored wicked configs to default

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,20 +8,6 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-if Rails.env.production?
-  WickedPdf.config = {
-    # Path to the wkhtmltopdf executable: This usually isn't needed if using
-    # one of the wkhtmltopdf-binary family of gems.
-    exe_path: '/home/ams/itdc_accounts/shared/bundle/ruby/2.3.0/bin/wkhtmltopdf',
-    #   or
-    # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-
-    # Layout file to be used for all PDFs
-    # (but can be overridden in `render :pdf` calls)
-    layout: 'pdf.html'
-  }
-else
-  WickedPdf.config = {
-    layout: 'pdf.html'
-  }
-end
+WickedPdf.config = {
+  layout: 'pdf.html'
+}
